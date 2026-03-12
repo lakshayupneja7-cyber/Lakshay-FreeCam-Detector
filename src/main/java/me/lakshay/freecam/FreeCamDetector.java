ab9c721ab4d68d71caad92d93bda22fe7e5a97a3
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-03-12 17:17:20 +0530

    Update FreeCamDetector.java

--- src/main/java/me/lakshay/freecam/FreeCamDetector.java
+++ src/main/java/me/lakshay/freecam/FreeCamDetector.java
@@ -6,18 +6,12 @@ public final class FreeCamDetector extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
         getLogger().info("Lakshay FreeCam Detector Enabled");
-
         new PacketListener(this).register();
-
     }
 
     @Override
     public void onDisable() {
-
         getLogger().info("Lakshay FreeCam Detector Disabled");
-
     }
-
 }
