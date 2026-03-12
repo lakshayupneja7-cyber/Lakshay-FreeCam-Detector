4e6e96ea0498d12a7903bc551f56e16f71d8d837
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-03-12 16:37:14 +0530

    Create FreeCamDetector.java

---
+++ src/main/java/me/lakshay/freecam/FreeCamDetector.java
@@ -0,0 +1,23 @@
+package me.lakshay.freecam;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FreeCamDetector extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        getLogger().info("Lakshay FreeCam Detector Enabled");
+
+        new PacketListener(this).register();
+
+    }
+
+    @Override
+    public void onDisable() {
+
+        getLogger().info("Lakshay FreeCam Detector Disabled");
+
+    }
+
+}
